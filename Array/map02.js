d2e83244d9ab49e9c47a161f3acc7418e6ec2095
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2021-11-30 07:38:11 +0200

    map2

---
+++ Array/map02.js
@@ -0,0 +1,14 @@
+const carrinho = [
+    '{ "nome": "Boracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Boracha", "preco": 7.50 }'
+]
+
+//retornar um array com os precos
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
