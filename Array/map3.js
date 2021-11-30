77234a049ce2109ecfaf975a0fc3bf8900db1e0f
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2021-11-30 07:48:20 +0200

    map3

---
+++ Array/map3.js
@@ -0,0 +1,22 @@
+Array.prototype.map2 = function (callback){
+    const newArray = []
+    for(let i = 0; i < this.length; i++){
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+} 
+
+const carrinho = [
+    '{ "nome": "Boracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Boracha", "preco": 7.50 }'
+]
+
+//retornar um array com os precos
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log(resultado)
