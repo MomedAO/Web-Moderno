30517a1268da495909a86fcb54d51ff4d746378a
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2021-12-02 05:11:45 +0200

    modulo no node

---
+++ node/moduloClient.js
@@ -0,0 +1,11 @@
+const moduleA = require('./moduloA')
+const moduleB = require('./moduleB')
+
+console.log(moduleA.ola)
+console.log(moduleA.bemVindo)
+console.log(moduleA.atelogo)
+console.log(moduleA)
+
+console.log(moduleB.bomDia)
+console.log(moduleB.boaNoite())
+console.log(moduleB)
