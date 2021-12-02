c5cf92d39a8ec08d72cd2c052b0637eac46307e2
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2021-12-02 07:17:00 +0200

    deletado

--- exerciciosFundamentos/calcularSalario.js
+++
@@ -1,9 +0,0 @@
-//funcao literal
-function calcularSalario(numHoras, valorHora){
-    console.log('Salario igual a R$ ' + valorHora * numHoras)
-}
-calcularSalario(150, 40.5)
-
-//funcao arrow
-let calcSalario = (nHoras, vHoras) => console.log('Salario igual a R$ ' + vHoras * nHoras)
-calcSalario(150, 40.5)
