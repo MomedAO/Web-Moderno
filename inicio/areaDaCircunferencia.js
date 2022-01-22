810c5d7dc1eefc7fc55e01ca4ace7cb72bfe896e
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2022-01-22 19:44:52 +0200

    recomecando com js

---
+++ inicio/areaDaCircunferencia.js
@@ -0,0 +1,4 @@
+const raio = 2;
+const areaDaCircunferencia = Math.PI * Math.pow(raio, 2);
+
+console.log('O resultado da area = ' + areaDaCircunferencia + 'm2');
---
+++ inicio/trocando.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 93;
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(`valor de a = ${a}`);
+console.log(`Valor de b = ${b}`);
