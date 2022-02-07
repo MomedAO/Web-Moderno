06052f2a5b472f5a7d61dd5105edc8ceb4256624
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2022-02-07 14:55:14 +0200

    ternario

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d;
+const subtracao = d - b;
+const muliplicacao = a * b;
+const divisao = d / a;
+const resto = a % 2;
+
+console.log(soma, subtracao, muliplicacao, divisao, resto);
---
+++ fundamentos/ternario.js
@@ -0,0 +1,3 @@
+const aprovado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado';
+
+console.log(aprovado(5));
