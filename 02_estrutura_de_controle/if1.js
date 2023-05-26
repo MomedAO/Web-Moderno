ec97f78987ee6563bd9789ca2b205a41136ac55e
MomedAO <momedabdalaa@gmail.com>
2023-05-26 05:00:51 +0200

    estrutura de controle

--- 02_estrutura_de_controle/if1.js
+++ 02_estrutura_de_controle/if1.js
@@ -6,3 +6,6 @@ function soBoaNoticia(nota) {
 soBoaNoticia(10)
 soBoaNoticia(6)
 soBoaNoticia(8)
+
+const good_news = nota => nota >= 7 ? `Aprovado com ${nota}` : `Reprovado com ${nota}`
+console.log(good_news(20))
